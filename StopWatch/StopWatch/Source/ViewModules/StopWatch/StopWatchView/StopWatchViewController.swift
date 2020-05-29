a39062f2fe60f46a76661e5e27a72d9bc9aba102
ginessan <ginesinho@gmail.com>
2020-05-29 12:33:34 +0200

    Add UI implementation (Laps not included). Add the view controller set up and the communication logic to his view model (view controller delegate).

--- StopWatch/StopWatch/Source/ViewModules/StopWatch/StopWatchView/StopWatchViewController.swift
+++ StopWatch/StopWatch/Source/ViewModules/StopWatch/StopWatchView/StopWatchViewController.swift
@@ -8,23 +8,72 @@
 
 import UIKit
 
+protocol StopWatchViewControllerDelegate: class {
+    func viewDidLoad()
+    func mainButtonTapped()
+    func secondaryButtonTapped()
+}
+
 final class StopWatchViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak var secondaryButton: UIButton!
+
+    var viewModel: StopWatchViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUp()
+
+        viewModel?.viewDidLoad()
+    }
+}
+
+// MARK: - IBActions
+extension StopWatchViewController {
+    @IBAction func mainButtonTapped(_ sender: Any) {
+        viewModel?.mainButtonTapped()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func secondaryButtonTapped(_ sender: Any) {
+        viewModel?.secondaryButtonTapped()
     }
-    */
+}
 
+// MARK: - Set Up Methods
+private extension StopWatchViewController {
+    //MARK: - Setup Methods
+    func setUp() {
+        setUpLabels()
+        setUpButtons()
+        navigationBar()
+    }
+
+    func setUpLabels() {
+        timeLabel.font = UIFont.monospacedSystemFont(ofSize: timeLabel.font.pointSize, weight: .semibold)
+        timeLabel.text = "00:00,00";
+    }
+
+    func setUpButtons() {
+        mainButton.setTitle("Start", for: .normal)
+        secondaryButton.setTitle("Reset", for: .normal)
+        secondaryButton.isEnabled = false
+
+        mainButton.backgroundColor = .lightGray
+        mainButton.layer.cornerRadius = 14
+        mainButton.layer.borderWidth = 2
+        mainButton.layer.borderColor = UIColor.black.cgColor
+
+        secondaryButton.backgroundColor = .lightGray
+        secondaryButton.layer.cornerRadius = 14
+        secondaryButton.layer.borderWidth = 2
+        secondaryButton.layer.borderColor = UIColor.black.cgColor
+    }
+
+    func navigationBar() {
+        self.title = "Timer";
+    }
 }
